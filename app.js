8337af9e88cb95bea80e9ce24cbc2e85027f59e3
Alex Rodriguez <drinkhorchata@gmail.com>
2017-07-29 22:03:32 -0500

    add pairWise challenge

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const funcs = require('./challenges');
 const cw = require('./codewars');
 
 console.log(
-  funcs.findLongestWord("The quick brown fox jumped over the lazy dog")
+  // funcs.findLongestWord("The quick brown fox jumped over the lazy dog")
+  funcs.pairwise([1,4,2,3,0,5], 7)
 );
